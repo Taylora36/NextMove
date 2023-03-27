96b283c070cf975e9ee65295253722e217a2c270
Taylora36 <taylor.anhalt36@gmail.com>
2023-03-27 00:39:24 +0000

    bare-bones

--- src/front/js/pages/Locations.jsx
+++ src/front/js/pages/Locations.jsx
@@ -1,34 +1,39 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-import { Card } from "../components/Card.jsx";
 import { Sidebar } from "../components/Sidebar.jsx";
+import { Card } from "../components/PlaceholderCard.jsx";
 import "../../styles/locations.css";
+import { BsFilter } from 'react-icons/bs';
+import { GoTriangleRight } from 'react-icons/go';
 
 export const Locations = () => {
     return(
-  <div className="_container">
+  <div className="whole_container">
     <Sidebar />
+    <div className="_container">
     <div className="top_row">
       <div className="title_page">
         <h1>Explore</h1>
       </div>
       <div className="_filter">
-        <button className="filter_button">Filter</button>
+        <button className="filter_button">Filter<span className="filter_symbol"><BsFilter/></span></button>
       </div>
     </div>
-    <div className="search_bar">
+    <form className="search_bar">
       <input className="search" placeholder="Search city..." type="text" />
-      <button className="search_button">Search</button>
-    </div>
-    <div className="card_carousel">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <div className="arrow">
-        <span>Arrow</span>
+      <button className="search_button">Go</button>
+    </form>
+    <div className="card_lineup">
+      <div className="_cards">
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+      <div className="_arrow">
+        <span><GoTriangleRight/></span>
       </div>
     </div>
+  </div>
   </div>
     )
 };
