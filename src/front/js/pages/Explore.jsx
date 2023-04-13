468c25282f28288249a5c9e0de3bb782fcc02288
Taylora36 <taylor.anhalt36@gmail.com>
2023-04-13 01:31:42 +0000

    Made it look much prettier

--- src/front/js/pages/Explore.jsx
+++ src/front/js/pages/Explore.jsx
@@ -4,31 +4,37 @@ import { Sidebar } from "../components/Sidebar.jsx";
 import { Card } from "../components/Card.jsx";
 import ExpandoCard from "../components/ExpandoCard.jsx";
 import "../../styles/explore.scss";
-import { BsFilter } from "react-icons/bs";
+import { GoTriangleDown } from "react-icons/go";
 import { IoIosArrowForward } from "react-icons/io";
 
 export const Explore = () => {
   return (
     <div className="locations_container">
-      <div className="top_row">
-        <div className="title_page">
-          <h1>Explore</h1>
-        </div>
-        <div className="_filter">
+      <div className="_filter">
+      <div class="drop_down">
           <button className="filter_button" role="button">
             Filter
-            <span className="filter_symbol">
-              <BsFilter />
-            </span>
+              <GoTriangleDown />
           </button>
+          <div class="dropdown_content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+          </div>
         </div>
+      <div className="top_row">
+        <div className="title_page">
+          <h3 className="title_header">Choose your city...</h3>
+        </div>
+        
       </div>
-      <form className="search_bar cf">
+      <div className="search_bar">
         <input className="search" placeholder="Search city..." type="text" />
         <button className="search_button" type="submit">
           Go
         </button>
-      </form>
+      </div>
       <div className="card_lineup">
         <Card />
         <button className="arrow_button">
