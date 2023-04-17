26a97b656c937db5d0b6aee9c2c06e30180d5017
Taylora36 <taylor.anhalt36@gmail.com>
2023-04-17 00:39:53 +0000

    added new card stuff

--- src/front/js/pages/Explore.jsx
+++ src/front/js/pages/Explore.jsx
@@ -4,7 +4,7 @@ import { Sidebar } from "../components/Sidebar.jsx";
 import { Card } from "../components/Card.jsx";
 import ExpandoCard from "../components/ExpandoCard.jsx";
 import "../../styles/explore.css";
-import { BsFilter } from "react-icons/bs";
+import { GoTriangleDown } from "react-icons/go";
 import { IoIosArrowForward } from "react-icons/io";
 
 export const Explore = () => {
@@ -14,30 +14,36 @@ export const Explore = () => {
     actions.getStateBatch();
     console.log(store.stateData);
   };
-
+  
   return (
     <div className="locations_container">
-      <div className="top_row">
-        <div className="title_page">
-          <h1>Explore</h1>
-        </div>
-        <div className="_filter">
+      <div className="_filter">
+      <div className="drop_down">
           <button className="filter_button" role="button">
             Filter
-            <span className="filter_symbol">
-              <BsFilter />
-            </span>
+              <GoTriangleDown />
           </button>
+          <div className="dropdown_content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+          </div>
         </div>
+      <div className="top_row">
+        <div className="title_page">
+          <h3 className="title_header">Choose your city...</h3>
+        </div>
+        
       </div>
-      <form className="search_bar cf">
+      <div className="search_bar">
         <input className="search" placeholder="Search city..." type="text" />
         <button className="search_button" type="submit">
           Go
         </button>
-      </form>
+      </div>
       <div className="card_lineup">
-        <div className="cards">
+      <div className="cards">
           {store.stateData.map((state, index) => {
             return <Card key={index} state={state} />;
           })}
@@ -46,7 +52,6 @@ export const Explore = () => {
           <IoIosArrowForward />
         </button>
       </div>
-
       <button
         style={{ position: "absolute", left: "150px" }}
         onClick={testState}
