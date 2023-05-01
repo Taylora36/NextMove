d0e7c59b9cd5f46e6a3e0f70af788dda9296e347
Taylora36 <taylor.anhalt36@gmail.com>
2023-05-01 16:40:15 +0000

    couple added changes

--- src/front/js/pages/Explore.jsx
+++ src/front/js/pages/Explore.jsx
@@ -22,6 +22,7 @@ export const Explore = () => {
   return (
     <div className="locations_container">
       <div className="top_row">
+        <span className="blob__left"></span>
         <div className="title_page">
           <h3 className="title_header">Choose your State</h3>
         </div>
